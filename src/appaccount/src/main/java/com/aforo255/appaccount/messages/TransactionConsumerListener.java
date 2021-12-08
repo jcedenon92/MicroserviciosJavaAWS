156e2482ab5ae508f2882504f1d7e2e8088b3a40
jcedenon92 <jcedenon92@gmail.com>
2021-12-08 02:53:47 -0500

    "Uso de Zipkin"

--- src/appaccount/src/main/java/com/aforo255/appaccount/messages/TransactionConsumerListener.java
+++ src/appaccount/src/main/java/com/aforo255/appaccount/messages/TransactionConsumerListener.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Component;
  
 import com.aforo255.appaccount.dtos.TransactionRequest;
 import com.aforo255.appaccount.models.AccountModel;
--- src/appmovements/src/main/java/com/aforo255/appmovements/cache/MovementRedisConfig.java
+++ src/appmovements/src/main/java/com/aforo255/appmovements/cache/MovementRedisConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
